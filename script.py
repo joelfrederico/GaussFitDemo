542f27b2928127811245d456b6b9c8e25fa8e250
Joel Frederico <joelfred@slac.stanford.edu>
2013-10-17 12:48:48 -0700

    Changed to line to test curve_fit more rigorously.

--- script.py
+++ script.py
@@ -5,68 +5,63 @@ import matplotlib.pyplot as plt
 import collections as col
 import scipy as sp
 import scipy.optimize as spopt
+import mytools as mt
 
 plt.close('all')
 
 def _gaussvar(x,amp,mu,variance,bg=0):
 	return amp*np.exp(-(x-mu)**2/(2*variance))+bg
 
+def line(x,b):
+	return b*np.ones(len(x))
+
 def iter(factor):
-	# Generate random numbers with gaussian
-	# distribution, mu=0, sigma = 3
-	x=np.random.randn(10000)*3
-	
-	# Bin the counts
-	bins = 50
-	h,edge = np.histogram(x,bins=bins)
+	# Generate a random-ish line from 0 to 1
+	num_pts = 1000
+	x = np.linspace(1,num_pts,num_pts)
+	y = 30 + np.random.randn(num_pts)*4
 
-	# Find the midpoints of the bins
-	mids = edge + (edge[1]-edge[0])/2
-	mids = mids[:-1]
-
-	# Expected error in counts is sqrt(counts)
-	sigma = np.sqrt(h)*1
-
-	# Error of zero counts isn't zero, but less than one.
-	# Use 0.5 as a guess.
-	sigma[sigma==0] = 0.5
+	# Expected error in counts is the sigma of the
+	# randomly generated points
+	sigma = 4 * np.ones(num_pts)
 
 	# Factor scales error, which should cause
 	# the pcov matrix to change
 	sigma = sigma*factor
 	
-	# Find initial guesses
-	y=h
-	x=mids
-	amp = max(y)
-	mu  = sum(x*y)/sum(y)
-	variance = sum(x**2 * y)/sum(y)
-	bg  = 0
-	p0 = np.array((amp,mu,variance,bg))
-	
+	# Initial guess close to 30
+	p0 = (25)
+
 	# Do actual curve fit
-	func = _gaussvar
+	func = line
 	popt,pcov = spopt.curve_fit(func,x,y,sigma=sigma,p0=p0)
 
-	output = col.namedtuple('iterout',['popt','pcov'])
-	out = output(popt,pcov)
+	output = col.namedtuple('iterout',['popt','pcov','x','y','sigma'])
+	out = output(popt,pcov,x,y,sigma)
 	return out
 
 num_samples       = 1000
 variances_regular = np.ones(num_samples)
-variances_large   = np.ones(num_samples)
+# variances_large   = np.ones(num_samples)
 means_regular     = np.ones(num_samples)
-means_large       = np.ones(num_samples)
+# means_large       = np.ones(num_samples)
 for i in np.linspace(1,num_samples,num_samples)-1:
 	out                  = iter(1)
-	means_regular[i]     = out.popt[2]
-	variances_regular[i] = out.pcov[2,2]
+	means_regular[i]     = out.popt[0]
+	variances_regular[i] = out.pcov[0,0]
 
-	out                = iter(1e10)
-	means_large[i]     = out.popt[2]
-	variances_large[i] = out.pcov[2,2]
+	# out                = iter(1e10)
+	# means_large[i]     = out.popt[0]
+	# variances_large[i] = out.pcov[0,0]
 
-plt.hist(variances_regular,bins=20)
-plt.hist(variances_large,bins=20)
+# plt.hist(variances_regular,bins=20)
+# plt.hist(variances_large,bins=20)
+
+print 'Average mean is {}.'.format(np.mean(means_regular))
+print 'Average variance is {}.'.format(np.mean(variances_regular))
+
+plt.hist(means_regular)
+
+print np.std(means_regular)
 
 plt.show()
